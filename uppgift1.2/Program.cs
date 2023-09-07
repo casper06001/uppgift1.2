090bd858fb1e5bf7b293520dbbf91dcc300f29f7
Pingvu <nhlpool27@gmail.com>
2023-09-07 13:14:13 +0200

    Add project files.

---
+++ uppgift1.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace uppgift1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Task failed successfully");
+            Console.ReadKey();
+        }
+    }
+}
